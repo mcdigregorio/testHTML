3a0f9e09ae8e9665541f2f2ea72bb96e26816739
DiGregorio <mcd0225@draper.com>
2019-05-06 16:49:12 -0400

    Full width graph and raised plant image

--- fs/js/waterGraph.js
+++ fs/js/waterGraph.js
@@ -5,7 +5,7 @@ var lenFive = lastFive.length;
 var number;
 														
 var data = {
-	labels: ['#1', '#2', '#3', '#4', '#5', '#6', '#7', '#8','#9', '#10'],
+	labels: ['1', '2', '3', '4', '5', '6', '7', '8','9', '10'],
 	series: [
 		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 	]
@@ -13,6 +13,7 @@ var data = {
 
 var options = {
 	//width: 500,
+	fullWidth: true,
 	height: 350,
 	low: 0,
 	showArea: true
@@ -34,7 +35,7 @@ setInterval(function(){
 	}
 	
 	var newData = {
-		labels: ['#1', '#2', '#3', '#4', '#5', '#6', '#7', '#8', '#9', '#10'],
+		labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
 		series: [
 			lastFive
 		]
